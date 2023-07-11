bb7d4f202e4d29e29aa33dec49d374301ac0c904
SuShruthi <shruthacm@gmail.com>
2023-07-11 00:27:09 -0700

    add helper functions

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -2,8 +2,12 @@ import React from "react"; //optional
 import playerData from '../data/playerData';
 import matchData from '../data/matchData';
 import Player from "./Player";
+import { preparePlayerData, addWinsToPlayers } from '../helpers/playerHelpers';
 
 function PlayerList(props) {
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayers(playerDataArray, matchData);
+  console.log(parsedPlayerData);
   return (
     <section className="PlayerList">
       <h1>Current participating players</h1>
